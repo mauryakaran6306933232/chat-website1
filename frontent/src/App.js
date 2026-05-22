8106de75c650403134916f651877124df39e31a5
nimasis brine <mauryakaran0542@gmail.com>
2026-05-22 18:45:08 +0530

    connect socket to render backend

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -1,29 +1,117 @@
 
-import React, { useEffect } from 'react'
-import Homepage from './components/Homepage'
-import Signup1 from './components/Signup1'
-import Login from './components/Login'
+// import React, { useEffect } from 'react'
+// import Homepage from './components/Homepage'
+// import Signup1 from './components/Signup1'
+// import Login from './components/Login'
+// import './App.css';
+// import { useSelector, useDispatch } from 'react-redux';
+// import io from 'socket.io-client'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { setSocket } from './redux/socketSlice';
+// import { setInitialUnreadCounts ,setOnlineUsers, setSelectedUser } from './redux/UserSlice';
+
+// // <--- IMPORT THE NEW PROTECTED ROUTE
+// import ProtectedRoute from './components/ProtectedRoute'; 
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/homepage',
+//     // <--- WRAP HOMEPAGE INSIDE THE PROTECTED ROUTE
+//     element: <ProtectedRoute><Homepage /></ProtectedRoute> 
+//   },
+//    {
+//     path: '/signup',
+//     element: <Signup1 />
+//   },
+//    {
+//     path: '/login',
+//     element: <Login />
+//   },
+//   {
+//     path: '/',
+//     element: <Login />
+//   }
+// ])
+
+// export default function App() {
+//   const { socket } = useSelector(store => store.socket);
+//   const dispatch = useDispatch();
+//   const { authUser } = useSelector(store => store.user);
+
+//   // Inside your App.js socket useEffect:
+// useEffect(() => {
+//   if (authUser) {
+//     const socket = io('http://localhost:8001', {
+//       query: { userId: authUser._id }
+//     });
+
+//     dispatch(setSocket(socket));
+
+//     socket.on('getOnlineUsers', (onlineUsers) => {
+//       dispatch(setOnlineUsers(onlineUsers));
+//     });
+//     socket.on('initial_unread_counts', (countsMap) => {
+//       dispatch(setInitialUnreadCounts(countsMap));
+//     });
+    
+//     // ■ CRITICAL: This forces the app to go back to the sidebar on refresh!
+//     dispatch(setSelectedUser(null)); 
+
+//     return () => socket.close();
+//   } else {
+//     if (socket) {
+//       socket.close();
+//       dispatch(setSocket(null));
+//     }
+//   }
+// }, [authUser, dispatch])
+
+//   return (
+//     <div className='flex h-screen items-center justify-center'>
+//       <RouterProvider router={router} />
+//     </div>
+//   )
+// }
+import React, { useEffect } from 'react';
+import Homepage from './components/Homepage';
+import Signup1 from './components/Signup1';
+import Login from './components/Login';
 import './App.css';
+
 import { useSelector, useDispatch } from 'react-redux';
-import io from 'socket.io-client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import io from 'socket.io-client';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { setSocket } from './redux/socketSlice';
-import { setInitialUnreadCounts ,setOnlineUsers, setSelectedUser } from './redux/UserSlice';
 
-// <--- IMPORT THE NEW PROTECTED ROUTE
-import ProtectedRoute from './components/ProtectedRoute'; 
+import {
+  setInitialUnreadCounts,
+  setOnlineUsers,
+  setSelectedUser
+} from './redux/UserSlice';
 
+// Protected Route
+import ProtectedRoute from './components/ProtectedRoute';
+
+// ================= API URL =================
+const API_URL = process.env.REACT_APP_API_URL;
+
+// ================= ROUTER =================
 const router = createBrowserRouter([
   {
     path: '/homepage',
-    // <--- WRAP HOMEPAGE INSIDE THE PROTECTED ROUTE
-    element: <ProtectedRoute><Homepage /></ProtectedRoute> 
+    element: (
+      <ProtectedRoute>
+        <Homepage />
+      </ProtectedRoute>
+    )
   },
-   {
+  {
     path: '/signup',
     element: <Signup1 />
   },
-   {
+  {
     path: '/login',
     element: <Login />
   },
@@ -31,44 +119,62 @@ const router = createBrowserRouter([
     path: '/',
     element: <Login />
   }
-])
+]);
 
+// ================= APP =================
 export default function App() {
-  const { socket } = useSelector(store => store.socket);
   const dispatch = useDispatch();
+
+  const { socket } = useSelector(store => store.socket);
   const { authUser } = useSelector(store => store.user);
 
-  // Inside your App.js socket useEffect:
-useEffect(() => {
-  if (authUser) {
-    const socket = io('http://localhost:8001', {
-      query: { userId: authUser._id }
-    });
+  useEffect(() => {
 
-    dispatch(setSocket(socket));
+    // ================= CONNECT SOCKET =================
+    if (authUser) {
 
-    socket.on('getOnlineUsers', (onlineUsers) => {
-      dispatch(setOnlineUsers(onlineUsers));
-    });
-    socket.on('initial_unread_counts', (countsMap) => {
-      dispatch(setInitialUnreadCounts(countsMap));
-    });
-    
-    // ■ CRITICAL: This forces the app to go back to the sidebar on refresh!
-    dispatch(setSelectedUser(null)); 
+      const socketInstance = io(API_URL, {
+        query: {
+          userId: authUser._id
+        },
+        transports: ['websocket']
+      });
 
-    return () => socket.close();
-  } else {
-    if (socket) {
-      socket.close();
-      dispatch(setSocket(null));
+      // Save socket in redux
+      dispatch(setSocket(socketInstance));
+
+      // ================= ONLINE USERS =================
+      socketInstance.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+
+      // ================= UNREAD COUNTS =================
+      socketInstance.on('initial_unread_counts', (countsMap) => {
+        dispatch(setInitialUnreadCounts(countsMap));
+      });
+
+      // ================= RESET SELECTED USER =================
+      dispatch(setSelectedUser(null));
+
+      // ================= CLEANUP =================
+      return () => {
+        socketInstance.close();
+      };
+
+    } else {
+
+      // Disconnect socket if user logout
+      if (socket) {
+        socket.close();
+        dispatch(setSocket(null));
+      }
     }
-  }
-}, [authUser, dispatch])
+
+  }, [authUser, dispatch]);
 
   return (
     <div className='flex h-screen items-center justify-center'>
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
